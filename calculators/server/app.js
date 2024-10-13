60a6473bb03a07a01437f20f2e957a719cc2a1d0
unknown <jesalthakar30@gmail.com>
2024-10-13 12:39:11 +0530

    removed app.use

--- calculators/server/app.js
+++ calculators/server/app.js
@@ -16,43 +16,42 @@ app.use(cors({
 }));
 
 
-app.use((req, res, next) => {
-    // Set the Access-Control-Allow-Origin header to allow requests from any origin
+/* app.use((req, res, next) => {
+   
     res.header("Access-Control-Allow-Origin", "*");
 
-    // Set the Access-Control-Allow-Methods header to allow specific HTTP methods
+ 
     res.header(
         "Access-Control-Allow-Methods",
         "PUT, OPTIONS, GET, POST, DELETE"
     );
 
-    // Set the Access-Control-Allow-Headers header to allow specific HTTP headers
+  
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, X-Access-Token, X-Key, Authorization"
     );
 
-    // Call the next middleware function in the stack
+    
     next();
-});
+}); */
 
 // Define your routes here
 // For example:
 
 
-app.listen(process.env.PORT || 5000)
+//app.listen(process.env.PORT || 5000)
 
 //dataBase Connection
 
 
 
 
-/* const uri = process.env.MONGODB_URI;
+const uri = "mongodb://localhost:27017/MyDataBase"
 console.log(uri);
 mongoose.connect(uri)
     .then((result) => app.listen(5000))
-    .catch((err) => console.log(err)) */
-
+    .catch((err) => console.log(err))
 //routes
 app.use(authroutes)
 
